126b658a987b4b156aca5aec4b412491a65c7ef2
DropdGG <andrejmikenkov@gmail.com>
2026-06-16 19:32:44 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,22 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from app.config import config
+import os
 
 def get_db_connection():
+    database_url = os.getenv("DATABASE_URL")
+    
+    if not database_url:
+        return psycopg2.connect(
+            host=config.DB_HOST,
+            port=config.DB_PORT,
+            database=config.DB_NAME,
+            user=config.DB_USER,
+            password=config.DB_PASSWORD,
+            cursor_factory=RealDictCursor
+        )
     return psycopg2.connect(
-        host=config.DB_HOST,
-        port=config.DB_PORT,
-        database=config.DB_NAME,
-        user=config.DB_USER,
-        password=config.DB_PASSWORD,
-        cursor_factory=RealDictCursor
+        database_url,
+        cursor_factory=RealDictCursor,
+        sslmode='require'
     )
