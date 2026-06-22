304ebadba634370f51607dd2ce76b0a7b8e70789
DropdGG <andrejmikenkov@gmail.com>
2026-06-22 15:44:28 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -14,6 +14,7 @@ def get_db_connection():
             user=config.DB_USER,
             password=config.DB_PASSWORD,
             cursor_factory=RealDictCursor
+            client_encoding='UTF8'
         )
     return psycopg2.connect(
         database_url,
